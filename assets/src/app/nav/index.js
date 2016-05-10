6c2fbd5db95090199db31caf89763297214c6934
evolvedus <evolvedus@gmail.com>
2016-05-10 16:28:17 -0400

    navcollapse

--- assets/src/app/nav/index.js
+++ assets/src/app/nav/index.js
@@ -7,10 +7,28 @@ angular.module( 'bidio.nav', [
    	$rootScope.$on("$stateChangeSuccess", function() {
         window.scrollTo(0, 0);
     });
-    $scope.isCollapsed = true;
-    $scope.collapse = function(){
-		  $scope.isCollapsed = true;
-		  console.log($scope.isCollapsed)
-    }
+})
+
+.directive('navCollapse', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var visible = false;
+
+            element.on('show.bs.collapse', function () {
+                visible = true;
+            });
+
+            element.on("hide.bs.collapse", function () {
+                visible = false;
+            });
+
+            element.on('click', function(event) {
+                if (visible && 'auto' == element.css('overflow-y')) {
+                    element.collapse('hide');
+                }
+            });
+        }
+    };
 });
 
