5f5fb02016496b1af4d9afa658a09b5b505663e1
Taylor <tbmcdonald31@gmail.com>
2016-08-14 15:06:02 -0700

    keys louise

--- config/env/development.js
+++ config/env/development.js
@@ -39,19 +39,59 @@ module.exports = {
     domain: 'bidio.co'
   },
 
+
+  //////////////////////////////////////////////
+  // DEVELOPMENT KEYS // DOMAIN = LOCALHOST:1337
+  // comment out for production
+  //////////////////////////////////////////////
+
+  // passport: {
+  //
+  //   twitter: {
+  //     options: {
+  //       consumerKey: 'KuCamyJMPULGv1IbLVzo8R3cZ',
+  //       consumerSecret: 'hoAuYeieYnPyCljJ2yesZpJCAtDcjpyd5fVeuRRK3bhuSw40mK'
+  //     }
+  //   },
+  //
+  //   facebook: {
+  //     options: {
+  //       clientID: '681990038623614',
+  //       clientSecret: '618cb0659b1d06ee73ed9cc30b157bc4',
+  //       //callbackURL: 'http://localhost:1337/dashboard/profile',
+  //       scope: ['email'], /* email is necessary for login behavior */
+  //       profileFields: ['displayName', 'email', 'link', 'picture.type(small)']
+  //     }
+  //   },
+  //
+  //   google: {
+  //     options: {
+  //       clientID: '339413134115-lf714so29eoj2oqoai24hrshit2pfit0.apps.googleusercontent.com',
+  //       clientSecret: 'dD8ortnHOe8SxBReatNjKNOP',
+  //       scope: ['email'] /* email is necessary for login behavior */
+  //     }
+  //   }
+  // }
+
+
+  ////////////////////////////////////////
+  // PRODUCTION KEYS AKA DOMAIN = BIDIO.CO
+  // comment out for development
+  ////////////////////////////////////////
+
   passport: {
 
     twitter: {
       options: {
-        consumerKey: 'KuCamyJMPULGv1IbLVzo8R3cZ',
-        consumerSecret: 'hoAuYeieYnPyCljJ2yesZpJCAtDcjpyd5fVeuRRK3bhuSw40mK'
+        consumerKey: 'pIzYvAQOTHiKlysvVC5m2IYTI',
+        consumerSecret: 'qzyJkjzOEVvttX0iu6ZON72BKZ4T0q0tXudYlUqHbtdPTgVArQ'
       }
     },
 
     facebook: {
       options: {
-        clientID: '681990038623614',
-        clientSecret: '618cb0659b1d06ee73ed9cc30b157bc4',
+        clientID: '629279003894718',
+        clientSecret: '9d1cbd6a42f185aeb94ee1aa40a324e9',
         //callbackURL: 'http://localhost:1337/dashboard/profile',
         scope: ['email'], /* email is necessary for login behavior */
         profileFields: ['displayName', 'email', 'link', 'picture.type(small)']
@@ -60,11 +100,14 @@ module.exports = {
 
     google: {
       options: {
-        clientID: '339413134115-lf714so29eoj2oqoai24hrshit2pfit0.apps.googleusercontent.com',
-        clientSecret: 'dD8ortnHOe8SxBReatNjKNOP',
+        clientID: '140697652385-7kf690p492kpr7pub7ni2f6322ui89od.apps.googleusercontent.com',
+        clientSecret: 'EdNhiGSZajsrKl8NOJQ0PPjq',
         scope: ['email'] /* email is necessary for login behavior */
       }
     }
   }
+
+
+
 
 };
