202f03082174657873944fb6b74e5820bf344d82
Trevor Overman <troverman@gmail.com>
2017-04-10 06:36:50 -0400

    twitter

--- assets/src/common/services/seo.js
+++ assets/src/common/services/seo.js
@@ -4,10 +4,11 @@ angular.module('services.seo', [])
   var keywords;
   return {
     setDescription: function(description) {
-      return $window.document.getElementsByName('description')[0].content = description;
+      $window.document.getElementsByName('description')[0].content = description;
+      $window.document.getElementsByName('twitter:description')[0].content = description;
     },
     setKeywords: function(keywords) {
-      return $window.document.getElementsByName('keywords')[0].content = keywords;
+      $window.document.getElementsByName('keywords')[0].content = keywords;
     }
   };
 }]);
--- assets/src/common/services/title.js
+++ assets/src/common/services/title.js
@@ -1,10 +1,11 @@
 angular.module('services.title', [])
-.factory('titleService',['$document', function($document) {
+.factory('titleService',['$document, $window', function($document, $window) {
   var title;
   return {
     setTitle: function(t) {
       title = t;
-      return $document.prop('title', title);
+      $document.prop('title', title);
+      $window.document.getElementsByName('twitter:title')[0].content = title;
     },
     getTitle: function() {
       return $document.prop('title');
