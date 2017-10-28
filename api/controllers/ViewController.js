05ea2ecaa15b2a93b3983d1bc75f8a7f7a89c366
Trevor Overman <troverman@gmail.com>
2017-10-28 07:22:40 -0400

    1

--- api/controllers/ViewController.js
+++ api/controllers/ViewController.js
@@ -38,93 +38,95 @@ module.exports = {
 			watchTime: req.param('watchTime'),
 		};
 
-		View.create(model)
-		.exec(function(err, model) {
-			if (err) {return console.log(err)}
-			else {
-				View.count().where({video: req.param('video')})
-				.exec(function(err, viewCount) {
-					Video.update({id: req.param('video')}, {viewCount: viewCount})
-					.then(function(result){
-						Video.getOne(result[0].id)
-						.then(function(video){
-							if (err) return res.json(err, 400);
-							Video.publishUpdate(video[0].id, video[0]);
-						});
-					})
-					.catch(function(err){
-						return res.negotiate(err);
-					})
+		if (req.user){
+			View.create(model)
+			.exec(function(err, model) {
+				if (err) {return console.log(err)}
+				else {
+					View.count().where({video: req.param('video')})
+					.exec(function(err, viewCount) {
+						Video.update({id: req.param('video')}, {viewCount: viewCount})
+						.then(function(result){
+							Video.getOne(result[0].id)
+							.then(function(video){
+								if (err) return res.json(err, 400);
+								Video.publishUpdate(video[0].id, video[0]);
+							});
+						})
+						.catch(function(err){
+							return res.negotiate(err);
+						})
 
-					//Bid.update({id: req.param('bid')}, {viewCount:viewCount}).exec(function afterwards(err, updated){
-						//if (err) {return;}
-						//else{Bid.publishUpdate(updated.toJSON())}
-					//});
-				});
-				//View.watch(req);
-				//View.publishCreate(model.toJSON());
-				//res.json(model);
+						//Bid.update({id: req.param('bid')}, {viewCount:viewCount}).exec(function afterwards(err, updated){
+							//if (err) {return;}
+							//else{Bid.publishUpdate(updated.toJSON())}
+						//});
+					});
+					//View.watch(req);
+					//View.publishCreate(model.toJSON());
+					//res.json(model);
 
-				var viewModel = {
-					//get dynamic milliseconds on close or max of content. 
-					watchTime: model.watchTime,
-					video: model.video,
-					user: req.user.walletAddress || null
-				};
+					var viewModel = {
+						//get dynamic milliseconds on close or max of content. 
+						watchTime: model.watchTime,
+						video: model.video,
+						user: req.user.walletAddress || null
+					};
 
-				//gotta work
-				//this method isnt really effecient ->re general, multidemsional viewtoken..
-				/*viewModel = {
-					watchTime: 60000,
-					content: model.video,
-					channel: model.video,
-				}*/
-				//total viewtoken balance --
+					//gotta work
+					//this method isnt really effecient ->re general, multidemsional viewtoken..
+					/*viewModel = {
+						watchTime: 60000,
+						content: model.video,
+						channel: model.video,
+					}*/
+					//total viewtoken balance --
 
-				//Legacy viewToken viewer
-		  		//blockchainService.createView(viewModel);
+					//Legacy viewToken viewer
+			  		//blockchainService.createView(viewModel);
 
-				//viewtokens
-				//-->identifer-->video, channel, general, viewer? 
-				//content
-				if (viewModel.watchTime != 0){
-					blockchainService.createMultiDimensionalViewToken(viewModel);
-				}
-				//channel
-				//blockchainService.createMultiDimensionalViewToken(viewModel);
-				//general
-				viewModel.video = 'general'
-				if (viewModel.watchTime != 0){
-					blockchainService.createMultiDimensionalViewToken(viewModel);
-				}
+					//viewtokens
+					//-->identifer-->video, channel, general, viewer? 
+					//content
+					if (viewModel.watchTime != 0){
+						blockchainService.createMultiDimensionalViewToken(viewModel);
+					}
+					//channel
+					//blockchainService.createMultiDimensionalViewToken(viewModel);
+					//general
+					viewModel.video = 'general'
+					if (viewModel.watchTime != 0){
+						blockchainService.createMultiDimensionalViewToken(viewModel);
+					}
 
+					
+					//blockchainService.createViewTESTNET(viewModel);
+
+			  		//viewToken creator
+			  		//channel token
+			  		Video.find({id:req.param('video')}).then(function(videoModel){
+			  			User.find({id:videoModel[0].user}).then(function(userModel){
+			  				var viewModelChannel = {
+								watchTime: model.watchTime,
+								video: userModel[0].walletAddress,
+								user: req.user.walletAddress
+							};
+							//var viewModelCreaterToken = {
+							//	watchTime: model.watchTime,
+							//	video: userModel[0].walletAddress,
+							//	user: userModel[0].walletAddress
+							//};
+							//channeltoken
+							console.log(viewModelChannel);
+							if (viewModelChannel.watchTime != 0){
+								blockchainService.createMultiDimensionalViewToken(viewModelChannel);
+							}
+							//blockchainService.createMultiDimensionalViewToken(viewModelCreaterToken);
+			  			});
+			  		});
 				
-				//blockchainService.createViewTESTNET(viewModel);
-
-		  		//viewToken creator
-		  		//channel token
-		  		Video.find({id:req.param('video')}).then(function(videoModel){
-		  			User.find({id:videoModel[0].user}).then(function(userModel){
-		  				var viewModelChannel = {
-							watchTime: model.watchTime,
-							video: userModel[0].walletAddress,
-							user: req.user.walletAddress
-						};
-						//var viewModelCreaterToken = {
-						//	watchTime: model.watchTime,
-						//	video: userModel[0].walletAddress,
-						//	user: userModel[0].walletAddress
-						//};
-						//channeltoken
-						console.log(viewModelChannel);
-						if (viewModelChannel.watchTime != 0){
-							blockchainService.createMultiDimensionalViewToken(viewModelChannel);
-						}
-						//blockchainService.createMultiDimensionalViewToken(viewModelCreaterToken);
-		  			});
-		  		});
-			
-			}
-		});
+				}
+			});
+		}
 	}
 };
