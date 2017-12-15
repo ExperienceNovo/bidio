5d6c32a5822410526d3c0879f1ab72b68cae0668
Trevor Overman <troverman@gmail.com>
2017-12-14 22:27:12 -0500

    updates n shuff

--- assets/src/app/token/index.js
+++ assets/src/app/token/index.js
@@ -15,4 +15,14 @@ angular.module( 'bidio.token', [
 
 .controller( 'TokenCtrl', ['$scope', 'titleService', function TokenController( $scope, titleService ) {
 	titleService.setTitle('CRE8BID.IO | CREATE THE NEXT WAVE');
+	$scope.totalEth  = ((Math.round(+new Date()/1000) - 1513305631)/60).toFixed(4);
+	$scope.tokenPrice = (Math.pow(2, Math.log($scope.totalEth*60)) - 2306958).toFixed(8);
+	$scope.totalToken =  Math.floor(100 * $scope.totalEth / $scope.tokenPrice);
+	$scope.getPrice = function(){
+		var unix = Math.round(+new Date()/1000);
+		var power = Math.log(unix)
+		$scope.tokenPrice = (Math.pow(2, power) - 2306958).toFixed(8);
+		$scope.totalToken =  Math.floor(100 * $scope.totalEth / $scope.tokenPrice);
+	};
+	$scope.getPrice();
 }]);
