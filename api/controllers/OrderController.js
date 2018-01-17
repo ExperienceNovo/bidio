b22424f9ba8950d8910e315453a54d8dd181d2f1
Trevor Overman <troverman@gmail.com>
2018-01-16 21:40:01 -0500

    fix

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -2,7 +2,7 @@ var crypto = require("crypto");
 module.exports = {
 	create: function(req, res) {
 		//TODO -- REMOVE BACKEND
-		blockchainService.createOrder(req.query.member, req.query.orderExchangeAmount, req.query.orderExchangeIdentifier, req.query.orderExchangeAmount1, req.query.orderExchangeIdentifier1)).then(function(model){
+		blockchainService.createOrder(req.query.member, req.query.orderExchangeAmount, req.query.orderExchangeIdentifier, req.query.orderExchangeAmount1, req.query.orderExchangeIdentifier1).then(function(model){
 			res.json(model);
 		});
 	},
