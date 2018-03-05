3f59d818d35bdb5fa74718c3b2cf0c56147c08ee
Trevor Overman <troverman@gmail.com>
2018-03-05 14:23:40 -0500

    protocol init

---
+++ api/contracts/personalIdentiferProtocol.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.0;
+contract personalIdentiferProtocol {
+    //only owner of wallet address can mint token with wallet identifer
+    //can only mint 86400 tokens per day
+    //based on datetime object for helper contract
+}
