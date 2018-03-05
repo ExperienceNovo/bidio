2bda2aa8108fef436b8431a2a82ebb8d5692f366
Trevor Overman <troverman@gmail.com>
2018-03-05 14:46:20 -0500

    content

--- api/contracts/contentContract.sol
+++ api/contracts/contentContract.sol
@@ -1,4 +1,17 @@
 pragma solidity ^0.4.0;
 contract content {
 
+	struct Content {
+		address member;
+        string title;
+        string description;
+        string url;
+    }
+    mapping (address => Content) public content;
+    event CreateContent(address indexed _member, string _title, string _description, string _url);
+
+    function createContent(address _member, string _title, string _description, string _url) {
+        CreateContent(_member, _title, _description, _url)
+    }
+
 }
