6ca99329c297f0615fd927053a3d58b218974465
Trevor Overman <troverman@gmail.com>
2018-03-05 14:25:37 -0500

    1

---
+++ api/contracts/contentIdentiferProtocol.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.0;
+contract contentIdentiferProtocol {
+    //only owner of content (linked content contract to address mapping) can mint token with content identifer
+    //is minted on view
+    //dileniate based on vT protocol ~ share percent etc
+}
