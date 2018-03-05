38cf076d392c9e82880375a437dfedd4de1a4439
Trevor Overman <troverman@gmail.com>
2018-03-05 15:13:49 -0500

    1

--- api/contracts/contentProtocol.sol
+++ api/contracts/contentProtocol.sol
@@ -1,5 +1,5 @@
-pragma solidity ^0.4.0;
-contract contentIdentiferProtocol {
+pragma solidity ^0.4.2;
+contract contentProtocol {
     //only owner of content (linked content contract to address mapping) can mint token with content identifer
     //is minted on view
     //dileniate based on vT protocol ~ share percent etc
--- api/contracts/personalProtocol.sol
+++ api/contracts/personalProtocol.sol
@@ -1,5 +1,5 @@
-pragma solidity ^0.4.0;
-contract personalIdentiferProtocol {
+pragma solidity ^0.4.2;
+contract personalProtocol {
     //only owner of wallet address can mint token with wallet identifer
     //can only mint 86400 tokens per day
     //based on datetime object for helper contract
