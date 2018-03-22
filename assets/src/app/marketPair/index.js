cb61a149e81c51febed67f67d37c4c11db03acb6
Trevor Overman <troverman@gmail.com>
2018-03-22 04:16:07 -0400

    price

--- assets/src/app/marketPair/index.js
+++ assets/src/app/marketPair/index.js
@@ -92,8 +92,8 @@ angular.module( 'bidio.marketPair', [
 
     for (var i;i>1000;i++){
     	 $scope.timeChart.series[0].data.push([x,Math.random()*x*100]);
-    	 $scope.timeChart.series[1].data.push([x,Math.random()*x*100])
-    	 $scope.timeChart.series[2].data.push([x,Math.random()*x*100])
+    	 $scope.timeChart.series[1].data.push([x,Math.random()*x*150])
+    	 $scope.timeChart.series[2].data.push([x,Math.random()*x*50])
     }
 
 
